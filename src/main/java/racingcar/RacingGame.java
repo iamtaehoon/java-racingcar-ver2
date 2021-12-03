35376be2709bda782bc07253a37402ade8aba626
iamtaehoon <kimth9981@naver.com>
2021-12-03 20:59:03 +0900

    refactor: 입력 예외 발생 시 입력을 다시 받는 방식을 while에서 함수 호출로 변경

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,8 +3,8 @@ package racingcar;
 import java.util.List;
 
 public class RacingGame {
-	//TODO : try/catch로 다시 입력받는거 InputView에서 처리하면 더 좋겠다. 검증을 어디서하지? 아니면 검증 클래스를 따로 만들거나
 	Race race = new Race();
+	Round round;
 	String input;
 
 	public void start() {
@@ -13,16 +13,8 @@ public class RacingGame {
 	}
 
 	private void playGame() {
-		while (true) {
-			try {
-				race.repeatGamePhase(new Round(input));
-				race.showWinners();
-				return;
-			} catch (IllegalArgumentException e) {
-				OutputView.showErrorMessage(e);
-				inputRoundCnt();
-			}
-		}
+		race.repeatGamePhase(round);
+		race.showWinners();
 	}
 
 	private void initializeGame() {
@@ -31,20 +23,24 @@ public class RacingGame {
 	}
 
 	private void inputRoundCnt() {
-		input = InputView.inputRoundCnt();
+		try {
+			input = InputView.inputRoundCnt();
+			round = new Round(input);
+		} catch (IllegalArgumentException e) {
+			OutputView.showErrorMessage(e);
+			inputRoundCnt();
+		}
 	}
 
 	private void inputCarsName() {
-		while (true) {
-			try {
-				input = InputView.inputCarsName();
-				List<String> names = StringUtils.parseByComma(input);
-				names.stream().forEach(name -> race.participate(new Car(name)));
-				return;
-			} catch (IllegalArgumentException e) {
-				race.clearIncorrectValues();
-				OutputView.showErrorMessage(e);
-			}
+		try {
+			input = InputView.inputCarsName();
+			List<String> names = StringUtils.parseByComma(input);
+			names.stream().forEach(name -> race.participate(new Car(name)));
+		} catch (IllegalArgumentException e) {
+			race.clearIncorrectValues();
+			OutputView.showErrorMessage(e);
+			inputCarsName();
 		}
 	}
 }
