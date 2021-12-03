68cd5af619d21745aebc7930bf74042f651b6381
iamtaehoon <kimth9981@naver.com>
2021-12-03 21:01:34 +0900

    fix: 자동차들의 이름을 1,2,3,,, 마지막이 공백으로 끝나는 경우 정상 로직이 작동되는 오류 해결

--- src/main/java/racingcar/StringUtils.java
+++ src/main/java/racingcar/StringUtils.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class StringUtils {
 	public static List<String> parseByComma(String input) {
+		if (input.endsWith(",")) {
+			throw new IllegalArgumentException("자동차의 이름으로 공백이 들어갈 수 없다.");
+		}
 		return Arrays.asList(input.split(","));
 	}
 
