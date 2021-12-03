e62c1c0121b7284c1e5c27da2c4ba22e6a8a162f
iamtaehoon <kimth9981@naver.com>
2021-12-03 16:43:50 +0900

    test(Race): 자동차들을 Race 객체에 저장

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -4,4 +4,14 @@ import java.util.ArrayList;
 
 public class Race {
 	ArrayList<Car> cars = new ArrayList<>();
+
+	public void participate(Car car) {
+		cars.add(car);
+	}
+
+	public ArrayList<String> showAllCarName() {
+		ArrayList<String> allCarsName = new ArrayList<>();
+		cars.stream().forEach(car -> allCarsName.add(car.getName()));
+		return allCarsName;
+	}
 }
--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,8 +1,26 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RaceTest {
 
-
+	@Test
+	void 참가하는_자동차들_모두_저장() {
+		Race race = new Race();
+		String[] names = {"pobi", "car2", "3car", "kimth"};
+		for (String name : names) {
+			race.participate(new Car(name));
+		}
+		ArrayList<String> allCarsName = race.showAllCarName();
+		assertThat(allCarsName).containsExactly("pobi", "car2", "3car", "kimth");
+		assertThat(allCarsName).hasSize(4);
+	}
 }
