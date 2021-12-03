9bd9f1d8d796437151025265651c7cf611277302
iamtaehoon <kimth9981@naver.com>
2021-12-03 15:01:16 +0900

    test: 이름이 5글자를 초과하는 경우 IllegalArgumentException을 발생

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,6 +5,9 @@ public class Car {
     private int position = 0;
 
     public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5글자 이하여야 한다.");
+        }
         this.name = name;
     }
 
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,6 +11,15 @@ class CarTest {
 	@ValueSource(strings = {"pobi","car2","3car","kimth"})
 	@DisplayName("이름이 조건을 만족할 때 정상적으로 생성되는가 확인한다.")
 	void 여러개_차_이름_입력(String name) {
-		Assertions.assertThat(new Car(name));
+		assertThat(new Car(name));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"kimth2","1234567"})
+	@DisplayName("이름이 5글자를 넘어가 조건을 위반할 때 예외를 발생시켜야 한다.")
+	void 차_이름_초과(String name) {
+		assertThatThrownBy(() -> new Car(name))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("자동차의 이름은 5글자 이하여야 한다.");
 	}
 }
